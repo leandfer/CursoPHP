1dd1f978a8e8710b36945b2af28da971ddc1950b
Leandro Ferreira <ledferr@gmail.com>
2020-02-22 23:06:05 -0300

    Atualizado

---
+++ variaveis/exemplo-03.php
@@ -0,0 +1,27 @@
+<?php
+    //
+    
+    $nome = "Hcode";
+    $site = 'www.hcode.com.br';
+    
+    $ano = 1996;
+    $salario = 5500.99;
+
+    $bloqueado  = false;
+    ////////////////////////////
+
+    $frutas = array("abacaxi","laranja", "manga");
+    echo $frutas[2];
+    echo "</br>";
+    //////////////////////////////
+    
+    $nascimento = new DateTime();
+  //  var_dump($nascimento);
+    /////////////////////////////
+
+    $arquivo = fopen("exemplo-03.php", "r");
+    var_dump($arquivo);
+
+
+
+?>
